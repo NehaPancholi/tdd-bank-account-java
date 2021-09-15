de7d31d19e908544ed1feb7a2c9c266091b56d61
NehaPancholi <neha.pancholi@gmail.com>
2021-09-15 08:28:19 -0400

    deposit, multiple deposit, withdraw

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -4,12 +4,36 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void startingBalanceIsZero() {
+        Account account = new Account();
+        assertEquals(account.balance,0);
     }
+
+    @Test
+    public void depositAnAmount() {
+        Account account = new Account();
+        account.deposit(100);
+        assertEquals(account.balance, 100);
+    }
+
+    @Test
+    public void depositMultipleAmount() {
+        Account account = new Account();
+        account.deposit(100);
+        account.deposit(200);
+        assertEquals(account.balance, 300);
+    }
+
+    @Test
+    public void withdrawanAmount() {
+        Account account = new Account();
+        account.withdraw(50);
+        assertEquals(account.balance, -50);
+    }
+
 }
